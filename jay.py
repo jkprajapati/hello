1679456d27dee5f45714f07c031e07bbc11b071d
jkprajapati <jaykrushnaprajapati@gmail.com>
2022-02-05 22:38:25 +0530

    jaykrushna changed it

---
+++ jay.py
@@ -0,0 +1,53 @@
+jay=634
+rutik=5444
+subham=90
+dipen=9999
+
+# test1=jay>rutik and jay>subham and jay>dipen
+# test2=rutik>subham and rutik>dipen
+# test3=subham>dipen
+
+if jay>rutik and jay>subham:
+    print("jay")
+elif rutik>subham and rutik>dipen:
+    print("rut")
+elif subham>dipen:
+    print("subham")
+else:
+    print("dipen")
+# if test1 == True:
+#     print("jay")
+# elif test2==True:
+#     print("rutvik")
+# elif test3==True:
+#     print("shubham")
+# else:
+#     print("deepen")
+# else:
+#     if rutik>subham and rutik>dipen:
+#         print("rut")
+    
+#     elif subham>dipen:
+#         print("subham")
+#     else:
+#         print("dipen")
+
+# if jay>rutik:
+#     if jay>subham:
+#         if jay>dipen:
+#             print("jay")
+#         else:
+#             print("dipen")
+#     else:
+#         print("shubham")
+
+# elif rutik>subham:
+#     if rutik>dipen:
+#       print("rutik")
+#     else:
+#         print("dipen")
+# elif subham>rutik :
+#     if subham>dipen:
+#         print("shubham")
+#     else:
+#         print("dipen")
