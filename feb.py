e36be3d30d6212c2ba64dfc78f3b58f08655b1a1
jkprajapati <jaykrushnaprajapati@gmail.com>
2022-02-06 21:27:18 +0530

    final changed

---
+++ feb.py
@@ -0,0 +1,8 @@
+f1="jaykrushna"
+f2="kru"
+if f2 in f1:
+    for i in f1:
+        print(True)
+else: 
+    print(False)
+    
--- jay.py
+++ jay.py
@@ -1,7 +1,7 @@
-jay=634
-rutik=5444
-subham=90
-dipen=9999
+# jay=634
+# rutik=5444
+# subham=90
+# dipen=9999
 
 # test1=jay>rutik and jay>subham and jay>dipen
 # test2=rutik>subham and rutik>dipen
@@ -19,14 +19,14 @@ dipen=9999
 # test2=rutik>subham and rutik>dipen
 # test3=subham>dipen
 
-if jay>rutik and jay>subham:
-    print("jay")
-elif rutik>subham and rutik>dipen:
-    print("rut")
-elif subham>dipen:
-    print("subham")
-else:
-    print("dipen")
+# if jay>rutik and jay>subham:
+#     print("jay")
+# elif rutik>subham and rutik>dipen:
+#     print("rut")
+# elif subham>dipen:
+#     print("subham")
+# else:
+#     print("dipen")
 # if test1 == True:
 #     print("jay")
 # elif test2==True:
@@ -63,3 +63,35 @@ else:
 #         print("shubham")
 #     else:
 #         print("dipen")
+
+
+
+y1=input("enter the string:")#heart
+y2=input("enter the string")#earth
+l1=[[i for i in y1]
+,[i for i in y2]]
+l1[0].sort()
+l1[1].sort()
+
+if l1[0]==l1[1]:
+    print("anagram")
+else:
+    print("not anagram")
+# c1=0
+# c2=0
+# if len(y1)==len(y2):
+#     for i in y1:
+#         if i in y2:
+#             c1+=1
+        
+#     for i in y2:
+#         if i in y1:
+#             c2+=1
+    
+#     if c1==c2:
+#         print("both strings are anagrams")
+#     else:
+#         print("both strings are not anagrams")
+        
+# else:
+#         print("both strings are not anagrams")
